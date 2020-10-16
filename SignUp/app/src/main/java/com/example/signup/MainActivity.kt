6a741e49a6879e9314bde4ea99db71f3da9c9d90
YEJIN-LILY <gristar98@naver.com>
2020-10-16 17:14:47 +0900

    [Week1]Upload Assignments

--- SignUp/app/src/main/java/com/example/signup/MainActivity.kt
+++ SignUp/app/src/main/java/com/example/signup/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.signup
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,6 +18,16 @@ class MainActivity : AppCompatActivity() {
             val intent= Intent(this,SignUpActivity::class.java)
             startActivityForResult(intent,100)
         }
+
+        if(MySharedPreferences.getUserId(this).isNullOrBlank() || MySharedPreferences.getUserPass(this).isNullOrBlank()) { //자동로그인x
+            Login()
+        }
+        else { // SharedPreferences 안에 값이 저장되어 있을 때 -> HomeActivity로 이동
+            Toast.makeText(this, "${MySharedPreferences.getUserId(this)}님 자동 로그인 되었습니다.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -27,6 +38,22 @@ class MainActivity : AppCompatActivity() {
                     EditText_ID.setText(data!!.getStringExtra("id").toString())
                     EditText_PW.setText(data!!.getStringExtra("pw").toString())
                 }
+            }
+        }
+    }
+
+    fun Login() {
+        Button_LogIn.setOnClickListener{
+            if(EditText_ID.text.isNullOrBlank() || EditText_PW.text.isNullOrBlank()) { //빈칸이 있을 경우
+                Toast.makeText(this, "아이디와 비밀번호를 모두 입력하세요", Toast.LENGTH_SHORT).show()
+            }
+            else { //처음 로그인
+                MySharedPreferences.setUserId(this, EditText_ID.text.toString())
+                MySharedPreferences.setUserPass(this, EditText_PW.text.toString())
+                Toast.makeText(this, "${MySharedPreferences.getUserId(this)}님 로그인 되었습니다.", Toast.LENGTH_SHORT).show()
+                var intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
             }
         }
     }
--- SignUp/app/src/main/java/com/example/signup/SignUpActivity.kt
+++ SignUp/app/src/main/java/com/example/signup/SignUpActivity.kt
@@ -13,26 +13,20 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        Button_SignUp.setOnClickListener{
-            val name=EditText_name?.text.toString()
-            val id=EditText_ID?.text.toString()
-            val pw=EditText_PW?.text.toString()
+        Button_SignUp.setOnClickListener {
+            val id = EditText_ID?.text.toString()
+            val pw = EditText_PW?.text.toString()
 
-            val len_name=name.length
-            val len_id=id.length
-            val len_pw=pw.length
-
-            if(len_name>0&&len_id>0&&len_pw>0) {
+            if (id.isNullOrBlank() || pw.isNullOrBlank()) {
+                Toast.makeText(this, "빈칸이 있습니다.", Toast.LENGTH_SHORT).show()
+            } else {
                 Toast.makeText(this, "회원가입이 완료되었습니다!", Toast.LENGTH_SHORT).show()
-                val intent=Intent()
-                intent.putExtra("id",id)
-                intent.putExtra("pw",pw)
-                setResult(Activity.RESULT_OK,intent)
+                val intent = Intent()
+                intent.putExtra("id", id)
+                intent.putExtra("pw", pw)
+                setResult(Activity.RESULT_OK, intent)
                 finish()
             }
-            else
-                Toast.makeText(this,"빈칸이 있습니다.",Toast.LENGTH_SHORT).show()
         }
     }
-
 }
